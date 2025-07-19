1c8dbc48f37d4f76c7198ef029104af45d271289
Dams4K <dams4k@users.noreply.github.com>
2025-07-19 12:50:40 +0200

    CCommand & CEnum

---
+++ src/main/java/fr/cerasus/cerasuscommands/CCommand.java
@@ -0,0 +1,7 @@
+package fr.cerasus.cerasuscommands;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
+
+public interface CCommand extends CommandExecutor, TabCompleter {
+}
---
+++ src/main/java/fr/cerasus/cerasuscommands/CerasusCommands.java
@@ -0,0 +1,5 @@
+package fr.cerasus.cerasuscommands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CerasusCommands extends JavaPlugin { }
